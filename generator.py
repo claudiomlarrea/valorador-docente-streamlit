9a1972017d1f2d58c793cca11bfeebb9eb532093
Claudio Larrea <investigacion@uccuyo.edu.ar>
2025-06-17 16:42:53 -0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,14 +1,1 @@
-import pandas as pd
-from io import BytesIO
 
-def generar_excel(df, total, categoria):
-    output = BytesIO()
-    with pd.ExcelWriter(output, engine="openpyxl") as writer:
-        df.to_excel(writer, index=False, sheet_name="Puntajes")
-        resumen = pd.DataFrame({
-            "Total de puntos": [total],
-            "Categoría asignada": [categoria]
-        })
-        resumen.to_excel(writer, index=False, sheet_name="Resumen")
-    output.seek(0)
-    return output
