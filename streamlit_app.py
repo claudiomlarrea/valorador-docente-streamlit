4cd9d2d3d32132094de4de713f2ba92fe0710b7d
Claudio Larrea <investigacion@uccuyo.edu.ar>
2025-06-17 09:25:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
+import streamlit as st
+
+st.title("🧠 Valorador Docente - Resolución 897")
+st.write("La app se ha desplegado correctamente. Ahora podés empezar a cargar funcionalidades.")
 # Streamlit main app
 print('Streamlit app placeholder')
