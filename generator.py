7b66a4b6b115fec4db7aecf8e5714bb1d4b3f9c7
Claudio Larrea <investigacion@uccuyo.edu.ar>
2025-06-17 08:53:52 -0300

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,15 +1,2 @@
-
-import pandas as pd
-from io import BytesIO
-
-def generar_excel(resultados, total, categoria, docente):
-    df = pd.DataFrame(resultados.items(), columns=["Ítem", "Puntaje"])
-    df.loc[len(df.index)] = ["TOTAL", total]
-    df.loc[len(df.index)] = ["CATEGORÍA", categoria]
-    df.loc[len(df.index)] = ["DOCENTE", docente]
-
-    output = BytesIO()
-    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
-        df.to_excel(writer, sheet_name="Resultados", index=False)
-    output.seek(0)
-    return output
+# Generador de Excel
+print('Excel generator')
